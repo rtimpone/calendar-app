1619a8748a97e7adbb04f3faa5efd6884bca42f6
Rob Timpone <rtimpone@gmail.com>
2018-06-30 10:45:22 -0500

    Adding frameworks for calendar UI and data source

---
+++ CalendarApp/CalendarKit/CalendarKit.h
@@ -0,0 +1,19 @@
+//
+//  CalendarKit.h
+//  CalendarKit
+//
+//  Created by Rob Timpone on 6/30/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarKit.
+FOUNDATION_EXPORT double CalendarKitVersionNumber;
+
+//! Project version string for CalendarKit.
+FOUNDATION_EXPORT const unsigned char CalendarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarKit/PublicHeader.h>
+
+
---
+++ CalendarApp/CalendarView/CalendarView.h
@@ -0,0 +1,19 @@
+//
+//  CalendarView.h
+//  CalendarView
+//
+//  Created by Rob Timpone on 6/30/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarView.
+FOUNDATION_EXPORT double CalendarViewVersionNumber;
+
+//! Project version string for CalendarView.
+FOUNDATION_EXPORT const unsigned char CalendarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarView/PublicHeader.h>
+
+
