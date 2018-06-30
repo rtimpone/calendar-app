fd8e52cbf6471c5b02eba3ce6ee7f059954f4f95
Rob Timpone <rtimpone@gmail.com>
2018-06-30 10:27:26 -0500

    Setting up new project

---
+++ CalendarApp/CalendarApp/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  CalendarApp
+//
+//  Created by Rob Timpone on 6/30/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
---
+++ CalendarApp/CalendarApp/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  CalendarApp
+//
+//  Created by Rob Timpone on 6/30/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
